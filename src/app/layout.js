19d88bc26886de81ea794b518179787de80d2234
Hasan Rafi Ahmed <rafi@Hasans-Mac-mini.local>
2026-03-08 12:43:45 +0600

    feat: implement contact page with form and FAQs, add Toaster component for notifications

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "@/contexts/AuthProvider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthProvider>
+          {children}
+          <Toaster />
+        </AuthProvider>
+
       </body>
     </html>
   );
